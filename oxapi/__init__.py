364ab8096975bcd73e739ce0ce6ff3d544989a77
Bernd Strebel (E6530) <bernd.strebel@googlemail.com>
2015-12-14 18:49:41 +0100

    setuptools

--- oxapi/__init__.py
+++ oxapi/__init__.py
@@ -7,14 +7,10 @@ __author__ = 'Bernd Strebel'
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 from .session import OxHttpAPI
-from .beans import OxBean
-from .beans import OxBeans
-from .folders import OxFolder
-from .folders import OxFolders
-from .tasks import OxTask
-from .tasks import OxTasks
-from .attachment import OxAttachment
-from .attachment import OxAttachments
+from .beans import OxBean, OxBeans
+from .folders import OxFolder, OxFolders
+from .tasks import OxTask, OxTasks
+from .attachment import OxAttachment, OxAttachments
 
 __all__ = [
 
